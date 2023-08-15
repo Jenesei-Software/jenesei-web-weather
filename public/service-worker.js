22e41b268e52d2b97cf42598070f9ae0b2583616
Cyril Strone <cyrilstrone@gmail.com>
2023-08-15 17:56:36 +0700

    service-worker in work 2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,33 +1,30 @@
-// service-worker.js
-function getEndpoint() {
-  return self.registration.pushManager
-    .getSubscription()
-    .then(function (subscription) {
-      if (subscription) {
-        return subscription.endpoint;
-      }
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
 
-      throw new Error("User not subscribed");
-    });
-}
+self.addEventListener("push", function (e) {
+  console.log("puuuuuush");
+  if (!(self.Notification && self.Notification.permission === "granted")) {
+    //notifications aren't supported or permission not granted!
+    console.log("nononono");
+    return;
+  }
 
-// Register event listener for the ‘push’ event.
-self.addEventListener("push", function (event) {
-  // Keep the service worker alive until the notification is created.
-  event.waitUntil(
-    getEndpoint()
-      .then(function (endpoint) {
-        // Retrieve the textual payload from the server using a GET request. We are using the endpoint as an unique ID of the user for simplicity.
-        return fetch("./getPayload?endpoint=" + endpoint);
+  if (e.data) {
+    let message = e.data.json();
+    e.waitUntil(
+      self.registration.showNotification(message.title, {
+        body: message.body,
+        icon: message.icon,
+        actions: message.actions,
       })
-      .then(function (response) {
-        return response.text();
-      })
-      .then(function (payload) {
-        // Show a notification with title ‘ServiceWorker Cookbook’ and use the payload as the body.
-        self.registration.showNotification("ServiceWorker Cookbook", {
-          body: payload,
-        });
-      })
-  );
+    );
+  }
 });
+
+self.addEventListener(
+  "notificationclick",
+  function (event) {
+    event.notification.close();
+  },
+  false
+);
