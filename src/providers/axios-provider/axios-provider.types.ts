dc6afa97b6491e5490575d74715e04aa6474a733
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2024-02-03 19:12:30 +0700

    Переписал все

--- src/providers/axios-provider/axios-provider.types.ts
+++ src/providers/axios-provider/axios-provider.types.ts
@@ -1,10 +1,5 @@
-import { ITokenData } from '@api/auth'
-
 export interface AxiosProviderProps {
   children: React.ReactNode
 }
 
-export interface AxiosContextProps {
-  isAuthorized: boolean
-  saveToLocalStorage: (data: ITokenData) => void
-}
+export interface AxiosContextProps {}
