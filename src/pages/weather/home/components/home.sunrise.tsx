74da6b4156538afad1a7747768ac3f0649d86eca
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2024-02-05 05:47:19 +0700

    A lot of logics

--- src/pages/weather/home/components/home.sunrise.tsx
+++ src/pages/weather/home/components/home.sunrise.tsx
@@ -1,8 +1,10 @@
+import { HomeSunriseProps } from '..'
 import { IconWeather } from '@assets/icons/icon-weather'
 import { LayoutWidget } from '@layouts/layout-widget'
 import { StyledInterR12 } from '@styles/fonts/inter'
+import { FC } from 'react'
 
-export const HomeSunrise = () => {
+export const HomeSunrise: FC<HomeSunriseProps> = () => {
   return (
     <LayoutWidget
       title={
