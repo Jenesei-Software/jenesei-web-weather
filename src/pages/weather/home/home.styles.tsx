f54563bd55071d3cf86c775be5a7cc16fda486b3
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2024-02-10 04:43:48 +0700

    A lot of logics

--- src/pages/weather/home/home.styles.tsx
+++ src/pages/weather/home/home.styles.tsx
@@ -206,7 +206,7 @@ export const HomeMapWrapper = styled.div`
 
 export const HomeMapIconWrapper = styled.div`
   position: absolute;
-  z-index: 100;
+  z-index: 9999;
   display: flex;
   align-items: center;
   justify-content: center;
